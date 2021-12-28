815d51914e37276ac399efa2f27a3dbc046e83ff
Andrik2002 <andrikkobrin@gmail.com>
2021-12-29 00:03:39 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,60 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+////Перше завдання
+//
+//        byte a1   = 8;
+//        short a2  = 50;
+//        int a3    = 10;
+//        long a4   = 100l;
+//        char a5   = '3';
+//
+//        float a6  = 30.1f;
+//        double a7 = 40.4;
+//
+//        boolean a8 = true;
+//
+//        System.out.println("byte = " + a1);
+//        System.out.println("short = " + a2);
+//        System.out.println("int = " + a3);
+//        System.out.println("long = " + a4);
+//        System.out.println("char = " + a5);
+//        System.out.println("float = " + a6);
+//        System.out.println("double = " + a7);
+//        System.out.println("boolean = " + a8);
+
+
+////Друге завдання
+//
+//        System.out.println("Byte максимальне значення = " + Byte.MAX_VALUE + "; Byte = мінімальне значення" + Byte.MIN_VALUE);
+//        System.out.println("Short максимальне значення = " + Short.MAX_VALUE + "; Short = мінімальне значення" + Short.MIN_VALUE);
+//        System.out.println("Integer максимальне значення = " + Integer.MAX_VALUE + "; Integer = мінімальне значення" + Integer.MIN_VALUE);
+//        System.out.println("Long максимальне значення = " + Long.MAX_VALUE + "; Long = мінімальне значення" + Long.MIN_VALUE);
+//        System.out.println("Character максимальне значення = " + Character.MAX_VALUE + "; Character = мінімальне значення" + Character.MIN_VALUE);
+//
+//
+//        System.out.println("Float максимальне значення = " + Float.MAX_VALUE + "; Float = мінімальне значення" + Float.MIN_VALUE);
+//        System.out.println("Double максимальне значення = " + Double.MAX_VALUE + "; Double = мінімальне значення" + Double.MIN_VALUE);
+
+
+
+////Третє завдання
+//
+//        int [] array1 = new int [] { 5, 25, 10, 11, 15, 44, 100, 50, 30, 66} ;
+//        int min, max;
+//        min = max = array1[0];
+//
+//        for (int i = 1; i < array1.length; i++) {
+//            if (array1[i] < min)
+//                min = array1[i];
+//            if(array1[i] > max){
+//                max = array1[i];
+//            }
+//        }
+//        System.out.println("мінімальне число: " + min + "; максимальне число: " + max);
+
+    }
+}
